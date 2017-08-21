6af5be5336d83aa18ba2b165ca413fd9244f6424
Austin Graham <austin.graham@nextthought.com>
2017-08-21 08:03:05 -0500

    Added db code for loading finance data

---
+++ nti/data/database/__init__.py
@@ -0,0 +1,33 @@
+from sqlalchemy import create_engine
+
+from sqlalchemy.ext.declarative import declarative_base
+
+from sqlalchemy.orm import sessionmaker
+
+# Base of sqlalchemy based MySQL tables
+PersistentBase = declarative_base()
+
+
+class AbstractDatabase():
+    """
+    Basic database operations
+    """
+    def __init__(self, conn_str):
+        self.engine = create_engine(
+            conn_str, echo=False)
+        self.session = sessionmaker(bind=self.engine)()
+
+    def engine(self):
+        return self.engine
+
+    def session(self):
+        return self.session
+
+class AbstractTable():
+    """
+    Basic Table Object
+    """
+    
+    def __init__(self, **kwargs):
+        for attr, value in kwargs.items():
+            setattr(self, attr, value)
