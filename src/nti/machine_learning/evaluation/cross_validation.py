020355071e362af056f7bfd5e504126a834800e7
Carlos Sanchez <papachoco@gmail.com>
2017-09-15 10:24:13 -0500

    pep8 formatting; __init__ modules are not declared namespace;

---
+++ src/nti/machine_learning/evaluation/cross_validation.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+.. $Id$
+"""
+
+from __future__ import print_function, absolute_import, division
+__docformat__ = "restructuredtext en"
+
+logger = __import__('logging').getLogger(__name__)
+
+from sklearn.cross_validation import cross_val_score
+
+
+class KFoldCrossValidation(object):
+
+    def __init__(self, estimator, feature_data, target, cv, scoring):
+        self.cv = cv
+        self.target = target
+        self.scoring = scoring
+        self.estimator = estimator
+        self.feature_data = feature_data
+
+    def compute_scores(self):
+        scores = cross_val_score(self.estimator,
+                                 self.feature_data,
+                                 self.target,
+                                 cv=self.cv,
+                                 scoring=self.scoring)
+        return scores
