0202dbd3b06adcbdbf348ed48c9de06c93aad97f
Egawati Panjei <egawati.panjei@gmail.com>
2017-09-15 22:50:57 +0700

    put the comment on class KFoldCrossValidation back

--- src/nti/machine_learning/evaluation/cross_validation.py
+++ src/nti/machine_learning/evaluation/cross_validation.py
@@ -18,6 +18,31 @@ from nti.machine_learning.evaluation.interfaces import IKFoldCrossValidation
 
 @interface.implementer(IKFoldCrossValidation)
 class KFoldCrossValidation(object):
+    """
+    This class will estimate likely performance of a machine learning model. 
+    It splits datasets into k equal partitions (folds).
+    For each iteration, a partition/fold serves as testing data and the rest as training data
+    Testing accuracy is calculated on each iteration.
+    The iteration is repeated until k times and each time different partition serves as testing data.
+    After k iteration, the average testing accuracy estimates the out-of sample accuracy.
+
+    It accepts parameters :
+    estimator : 
+        machine learning type 
+        (could be Neural Network, k-Nearest Neighbor Classfifier, Linear Regressor, etc)
+    feature_data : 
+        the independent variable X
+    target : 
+        the dependent variable y
+    cv:
+        the number of k fold cross validation (generally cv is set to 10 --> 10 fold cross validation)
+    scoring:
+        the metric used to evaluate the performance. 
+        When dealing with classification, the metric could be classification accuracy, precision, ROC/AUC etc.
+        The score is calculated using the function cross_val_score (imported from sklearn.cross_validation),
+        the scoring value for classification accuracy = 'accuracy',
+        the scoring value for ROC/AUC = 'roc_auc'
+    """
 
     def __init__(self, estimator, feature_data, target, cv, scoring):
         self.cv = cv
