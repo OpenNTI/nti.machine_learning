233c07c27ac9de08c2d492c86d0b792ed557d909
Carlos Sanchez <papachoco@gmail.com>
2017-09-19 09:45:31 -0500

    pep8

--- src/nti/machine_learning/preprocessing/tests/test_encoding.py
+++ src/nti/machine_learning/preprocessing/tests/test_encoding.py
@@ -9,46 +9,42 @@ __docformat__ = "restructuredtext en"
 
 from hamcrest import is_
 from hamcrest import assert_that
-from hamcrest import less_than
-
-from nti.testing.matchers import validly_provides
 
 from nti.machine_learning.preprocessing.encoding import Encoder
 
 from nti.machine_learning.preprocessing.tests import EncoderLayerTest
 
+
 class TestEncoder(EncoderLayerTest):
-	def test_label_encoder_numeric(self):
-		label_encoder = Encoder(self.original_array_numeric, 'label')
 
-		assert_that(label_encoder.original_array[0], is_(1))
-		assert_that(label_encoder.original_array[1], is_(2))
-		assert_that(label_encoder.original_array[2], is_(2))
-		assert_that(label_encoder.original_array[3], is_(6))
+    def test_label_encoder_numeric(self):
+        label_encoder = Encoder(self.original_array_numeric, 'label')
 
-		transformed_array = label_encoder.transform(self.original_array_numeric)
-		
-		assert_that(transformed_array[0], is_(0))
-		assert_that(transformed_array[1], is_(1))
-		assert_that(transformed_array[2], is_(1))
-		assert_that(transformed_array[3], is_(2))
+        assert_that(label_encoder.original_array[0], is_(1))
+        assert_that(label_encoder.original_array[1], is_(2))
+        assert_that(label_encoder.original_array[2], is_(2))
+        assert_that(label_encoder.original_array[3], is_(6))
 
-	def test_label_encoder_nonnumeric(self):
-		label_encoder = Encoder(self.original_array_nonnumeric, 'label')
-	
-		assert_that(label_encoder.original_array[0], is_("barcelona"))
-		assert_that(label_encoder.original_array[1], is_("rome"))
-		assert_that(label_encoder.original_array[2], is_("sf"))
-		assert_that(label_encoder.original_array[3], is_("jakarta"))
-		assert_that(label_encoder.original_array[4], is_("sf"))
+        transformed_array = label_encoder.transform(self.original_array_numeric)
 
-		transformed_array = label_encoder.transform(self.original_array_nonnumeric)
-		
-		assert_that(transformed_array[0], is_(0))
-		assert_that(transformed_array[1], is_(2))
-		assert_that(transformed_array[2], is_(3))
-		assert_that(transformed_array[3], is_(1))
-		assert_that(transformed_array[4], is_(3))
+        assert_that(transformed_array[0], is_(0))
+        assert_that(transformed_array[1], is_(1))
+        assert_that(transformed_array[2], is_(1))
+        assert_that(transformed_array[3], is_(2))
 
+    def test_label_encoder_nonnumeric(self):
+        label_encoder = Encoder(self.original_array_nonnumeric, 'label')
 
+        assert_that(label_encoder.original_array[0], is_("barcelona"))
+        assert_that(label_encoder.original_array[1], is_("rome"))
+        assert_that(label_encoder.original_array[2], is_("sf"))
+        assert_that(label_encoder.original_array[3], is_("jakarta"))
+        assert_that(label_encoder.original_array[4], is_("sf"))
 
+        transformed_array = label_encoder.transform(self.original_array_nonnumeric)
+
+        assert_that(transformed_array[0], is_(0))
+        assert_that(transformed_array[1], is_(2))
+        assert_that(transformed_array[2], is_(3))
+        assert_that(transformed_array[3], is_(1))
+        assert_that(transformed_array[4], is_(3))
